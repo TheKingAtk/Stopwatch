3fb89836a18e3179cf0d086edd69c5699756dc1f
Bukki Atkinson <111119021@nitt.edu>
2020-08-21 10:46:27 +0530

    First commit

---
+++ app/src/main/java/com/theking/analoguestopwatch/StopwatchDial.java
@@ -0,0 +1,81 @@
+package com.theking.analoguestopwatch;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.shapes.ArcShape;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import java.util.logging.LogRecord;
+
+public class StopwatchDial extends View {
+    int h,w,stop=0;
+    int sec=0,min=0,hr=0;
+    int secAngle=90,minAngle=30;
+    Paint paint =new Paint(),minPaint=new Paint();
+    RectF rectF=new RectF(w/2,w/2,w/4+100,w/4+100);
+    public StopwatchDial(Context context) {
+        super(context);
+    }
+
+    public StopwatchDial(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public StopwatchDial(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        paint.setColor(Color.CYAN);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(50);
+        h=MeasureSpec.getSize(heightMeasureSpec);
+        w=MeasureSpec.getSize(widthMeasureSpec);
+        minPaint.setColor(Color.RED);
+        minPaint.setStyle(Paint.Style.STROKE);
+        minPaint.setStrokeWidth(50);
+        timer();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawArc(w/2-250,w/4,w/2+250,w/4+500,270,-(360-secAngle),false,paint);
+        canvas.drawArc(w/2-150,w/4+200,w/2+150,w/4+500,270,-(360-minAngle),false,minPaint);
+
+    }
+    public void timer(){
+        final Handler handler=new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+               sec++;
+               if(sec==60) {
+                   sec=0;
+                   min++;
+                   if(min==60) {
+                       min=0;
+                       hr++;
+                   }
+               }
+               secAngle=sec*6;
+               minAngle=min*6;
+               invalidate();
+               handler.postDelayed(new Runnable() {
+                   @Override
+                   public void run() {
+                       timer();
+                   }
+               },1000);
+            }
+        });
+    }
+}
